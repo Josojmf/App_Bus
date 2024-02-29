408d093b86a4a5971187f36cf748ebb6280b619e
Josojmf <joso.jmf@gmail.com>
2024-02-29 11:26:36 +0100

    commit inicial

---
+++ routes/Parada/index.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "https://esm.sh/v128/preact@10.19.2/src/index.js";
+import { type PageProps } from "$fresh/server.ts";
+type data = {
+    // 
+}
+
+type Res={
+
+}
+
